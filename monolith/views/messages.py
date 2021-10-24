047b03717a962cf77ae203af90cbede63967f12e
gystemd <gystemd@gmail.com>
2021-10-25 01:32:37 +0200

    added UI of send message

--- monolith/views/messages.py
+++ monolith/views/messages.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, redirect
 from dateutil import parser
+from flask.templating import render_template
 from monolith.background import send_message
+import datetime
 messages = Blueprint('messages', __name__)
 
 
-@messages.route('/message/send', methods = ['POST'])
+@messages.route('/message/send', methods = ['GET','POST'])
 def sendMessage():
-    data = request.get_json()
-    date = parser.parse(data['date'])
-    result = send_message.apply_async((data,), eta = date)
-    return jsonify({"msg":"Message sent!"})
 
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        date = parser.parse(request.form['date']+'+0200')
+        result = send_message.apply_async((data,), eta = date)
+        return render_template("send_message.html", message_ok = True)
+    else:
+        return render_template("send_message.html")
+
+@messages.route('/show/message', methods = ['GET'])
+def show_page():
+    return render_template("send_message.html")
