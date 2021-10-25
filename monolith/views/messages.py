948fcfd95e36b644dd675e0c5025dcdb3ef601d0
pacant <pacant2@gmail.com>
2021-10-25 12:03:46 +0200

    return template fixed

--- monolith/views/messages.py
+++ monolith/views/messages.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from dateutil import parser
+from flask.templating import render_template
 from monolith.background import send_message
+from monolith.database import User, db
 messages = Blueprint('messages', __name__)
 
 
@@ -11,3 +13,12 @@ def sendMessage():
     result = send_message.apply_async((data,), eta = date)
     return jsonify({"msg":"Message sent!"})
 
+@messages.route("/message/recipients", methods =["GET","POST"])
+def chooseRecipient():
+    if request.method == "GET":
+        email = session["email"]
+        recipients = db.session.query(User).filter(User.email != email)
+        return render_template("recipients.html", recipients=recipients)
+    if request.method == "POST":
+        recipient = request.form.get("select1")
+        return render_template("index.html", recipient=recipient)
