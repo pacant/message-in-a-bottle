85ff128ef45ad366d413c153b817348f68620aa0
fcarli3 <francescocarli79@gmail.com>
2021-10-25 11:27:02 +0200

    added mailbox story

--- monolith/views/__init__.py
+++ monolith/views/__init__.py
@@ -2,5 +2,6 @@ from .auth import auth
 from .home import home
 from .users import users
 from .messages import messages
+from .mailbox import mailbox
 
-blueprints = [home, auth, users, messages]
+blueprints = [home, auth, users, messages, mailbox]
