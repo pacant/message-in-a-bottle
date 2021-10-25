6dee59d9f55f21e2e8fa5850c9a1b0da571b4583
gystemd <gystemd@gmail.com>
2021-10-25 21:47:27 +0200

    Authentication required to send a message

--- monolith/views/messages.py
+++ monolith/views/messages.py
@@ -9,20 +9,22 @@ messages = Blueprint('messages', __name__)
 
 @messages.route('/message/send', methods = ['GET','POST'])
 def sendMessage():
-
-    if request.method == 'POST':
-        data = request.form
-        print(data)
-        date = parser.parse(request.form['date']+'+0200')
-        message = {
-            "text":data['text'],
-            "id_sender":current_user.id,
-            "receiver":data['receiver']
-        }
-        result = send_message.apply_async((message,), eta = date)
-        return render_template("send_message.html", message_ok = True)
+    if current_user is not None and hasattr(current_user, 'id'):
+        if request.method == 'POST':
+            data = request.form
+            print(data)
+            date = parser.parse(request.form['date']+'+0200')
+            message = {
+                "text":data['text'],
+                "id_sender":current_user.id,
+                "receiver":data['receiver']
+            }
+            result = send_message.apply_async((message,), eta = date)
+            return render_template("send_message.html", message_ok = True)
+        else:
+            return render_template("send_message.html")
     else:
-        return render_template("send_message.html")
+        return redirect('/')
 
 @messages.route('/show/message', methods = ['GET'])
 def show_page():
