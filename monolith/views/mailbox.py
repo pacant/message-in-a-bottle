73b36ba6ea98dc714a320102f5efe6a442a1b185
fcarli3 <francescocarli79@gmail.com>
2021-10-25 15:01:39 +0200

    fixes

--- monolith/views/mailbox.py
+++ monolith/views/mailbox.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, redirect
+from flask import Blueprint, request, jsonify, session
 from flask.templating import render_template
 from flask_login import current_user
 from monolith.database import Message, db
@@ -8,11 +8,7 @@ mailbox = Blueprint('mailbox', __name__)
 @mailbox.route('/mailbox/sent', methods = ['GET'])
 def see_sent_messages(): 
     if request.method == 'GET':
-        if current_user.is_authenticated:
-            user_id = current_user.get_id()
-            print("ID: " + str(current_user.id))
-            sent_msgs = db.session.query(Message).filter(Message.sender==) 
-            print(sent_msgs)
+        sent_msgs = db.session.query(Message).filter(Message.sender==current_user.id)
         return render_template('mailbox.html', sent_msgs=sent_msgs, view=True)
     else:
         return render_template('mailbox.html', view=True)
@@ -21,7 +17,7 @@ def see_sent_messages():
 @mailbox.route('/mailbox/received', methods = ['GET'])
 def see_received_messages():
     if request.method == 'GET':
-        rcv_msgs = db.session.query(Message).filter(Message.receiver==current_user.id) 
+        rcv_msgs = db.session.query(Message).filter(Message.receiver==current_user.id)
         return render_template('mailbox.html', rcv_msgs=rcv_msgs, view=False)
     else:
         return render_template('mailbox.html', view=False)
