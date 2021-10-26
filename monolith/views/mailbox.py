ad159d33cd77f6c3b7e90b64123d17b7dacbfb24
fcarli3 <francescocarli79@gmail.com>
2021-10-26 16:27:17 +0200

    fix bugs

--- monolith/views/mailbox.py
+++ monolith/views/mailbox.py
@@ -9,7 +9,7 @@ mailbox = Blueprint('mailbox', __name__)
 @mailbox.route('/mailbox/sent', methods = ['GET'])
 def see_sent_messages(): 
     if request.method == 'GET':
-        msgs_sent = db.session.query(Message, User).filter(Message.id_sender==User.id).filter(Message.id_sender==current_user.id).all()
+        msgs_sent = db.session.query(Message, User).filter(Message.id_receiver==User.id).filter(Message.id_sender==current_user.id).all()
         return render_template('msgs_sent.html', msgs_sent=msgs_sent)
     else:
         return render_template('msgs_sent.html')
@@ -18,7 +18,7 @@ def see_sent_messages():
 @mailbox.route('/mailbox/received', methods = ['GET'])
 def see_received_messages():
     if request.method == 'GET':
-        msgs_rcv = db.session.query(Message, User).filter(Message.id_receiver==User.id).filter(Message.id_receiver==current_user.id).all()
+        msgs_rcv = db.session.query(Message, User).filter(Message.id_sender==User.id).filter(Message.id_receiver==current_user.id).all()
         return render_template('msgs_rcv.html', msgs_rcv=msgs_rcv)
     else:
         return render_template('msgs_rcv.html')
@@ -27,7 +27,10 @@ def see_received_messages():
 @mailbox.route('/mailbox/draft', methods = ['GET'])
 def see_draft_messages():
     if request.method == 'GET':
-        draft_msgs = db.session.query(Message, User).filter(Message.id_receiver==User.id).filter(Message.id_receiver==current_user.id).filter(Message.draft==True).all()
+        draft_msgs = db.session.query(Message, User).filter(Message.id_receiver==User.id).filter(Message.id_sender==current_user.id).filter(Message.draft==True)
+        print(draft_msgs)
+        draft_msgs = draft_msgs.all()
+        print(draft_msgs)
         return render_template('msgs_draft.html', draft_msgs=draft_msgs)
     else:
         return render_template('msgs_draft.html')
