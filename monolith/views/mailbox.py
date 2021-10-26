c958791c9d376a7b30afa330218814311cb1240f
fcarli3 <francescocarli79@gmail.com>
2021-10-26 15:47:04 +0200

    added join query

--- monolith/views/mailbox.py
+++ monolith/views/mailbox.py
@@ -1,14 +1,18 @@
+from operator import and_
 from flask import Blueprint, request, jsonify, session
 from flask.templating import render_template
 from flask_login import current_user
-from monolith.database import Message, db
+from monolith.database import User, Message, db
+from sqlalchemy import and_
 
 mailbox = Blueprint('mailbox', __name__)
 
 @mailbox.route('/mailbox/sent', methods = ['GET'])
 def see_sent_messages(): 
     if request.method == 'GET':
-        msgs_sent = db.session.query(Message).filter(Message.id_sender==current_user.id)
+        #msgs_sent = db.session.query(Message, User).join(User, and_(Message.id_sender==User.id)).filter(Message.id_sender==current_user.id)
+        msgs_sent = db.session.query(Message, User).filter(Message.id_sender==User.id).filter(Message.id_sender==current_user.id).all()
+        print(msgs_sent)
         return render_template('msgs_sent.html', msgs_sent=msgs_sent)
     else:
         return render_template('msgs_sent.html')
@@ -17,7 +21,8 @@ def see_sent_messages():
 @mailbox.route('/mailbox/received', methods = ['GET'])
 def see_received_messages():
     if request.method == 'GET':
-        msgs_rcv = db.session.query(Message).filter(Message.id_receiver==current_user.id)
+        msgs_rcv = db.session.query(Message, User).join(User, and_(Message.id_receiver==User.id)).filter(Message.id_receiver==current_user.id)
+        print(msgs_rcv)
         return render_template('msgs_rcv.html', msgs_rcv=msgs_rcv)
     else:
         return render_template('msgs_rcv.html')
