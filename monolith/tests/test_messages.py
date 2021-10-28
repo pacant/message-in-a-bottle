45bf28ce67367877356ac4a909aefb86f761c3ab
gystemd <gystemd@gmail.com>
2021-10-28 14:54:43 +0200

    Added test for send_message

--- monolith/tests/test_messages.py
+++ monolith/tests/test_messages.py
@@ -1,3 +1,4 @@
+
 from datetime import date
 import json
 import unittest
@@ -28,23 +29,6 @@ class TestApp(unittest.TestCase):
                            ),
                            follow_redirects=True)
 
-    def unregister(self, client):
-        return client.post("/delete_user")
-
-    def test_auth(self):
-        tested_app.config['WTF_CSRF_ENABLED'] = False
-        app = tested_app.test_client()
-
-        reply = app.get("/create_user")
-        self.assertEqual(reply.status, '200 OK')
-
-        reply = self.register(app, "prova_003@example.it", "Prova", "Example", "1234", "01/01/2001")
-        self.assertEqual(reply.status, '200 OK')
-
-        reply = self.login(app, "prova_003@example.it", "1234")
-
-        self.assertIn(b"Hi", reply.data)
-
     def test_send_message(self):
         tested_app.config['WTF_CSRF_ENABLED'] = False
         app = tested_app.test_client()
@@ -65,5 +49,4 @@ class TestApp(unittest.TestCase):
                     ))
 
         self.assertIn(b"Message sent correctly!", reply.data)
-
-        db.session.query(User).filter( User.email == "prova_002@example.it")
+        User.query.filter_by(email="prova_001@example.it").delete()
